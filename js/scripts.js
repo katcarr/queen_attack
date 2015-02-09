5cc6781bbf064f1653c70e1b33611f0f7e00961c
Christopher Joy-Webb and Kathryn Carr <kathrynelizabethcarr@gmail.com>
2015-02-09 13:52:20 -0800

    add html and web application with logic integration

--- js/scripts.js
+++ js/scripts.js
@@ -7,3 +7,28 @@ var queenAttack = function(queen_position, piece_position){
     return false;
   }
 };
+
+$(function() {
+  $("form#positions").submit(function(event) {
+    var queenInput = $("input#queen").val();
+    var pieceInput = $("input#piece").val();
+
+    var queenPosition = [];
+    queenPosition.push(parseInt(queenInput[0]));
+    queenPosition.push(parseInt(queenInput[2]));
+
+    var piecePosition = [];
+    piecePosition.push(parseInt(pieceInput[0]));
+    piecePosition.push(parseInt(pieceInput[2]));
+
+    if(queenAttack(queenPosition, piecePosition)){
+      $("#not").hide();
+    }else{
+      $("#not").text("not");
+    }
+
+    $("#queen-position").text(queenInput);
+    $("#result").show();
+    event.preventDefault();
+  });
+});
